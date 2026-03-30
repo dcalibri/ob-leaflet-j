b129224f76695b0cb1f1db06501595736a8d2088
fsl_eightvram\fadhlisl <dcalibri@gmail.com>
2026-03-30 19:53:38 +0700

    update vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    environment: "node",
-    include: ["test/**/*.test.ts"],
-    reporters: ["default"],
+    globals: true,
+    typecheck: {
+      tsconfig: "./tsconfig.test.json",
+    },
   },
 });
